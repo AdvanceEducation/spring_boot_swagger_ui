3ca00bbb2048bfe93f3db2c59f2374624a64c373
William Santasiero <wsantasiero@advanc-ed.org>
2015-03-19 15:11:22 -0400

    correct static resources for jar

--- src/main/java/org/advanced/swaggerui/config/StaticResourceConfiguration.java
+++ src/main/java/org/advanced/swaggerui/config/StaticResourceConfiguration.java
@@ -8,8 +8,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/docs/**").addResourceLocations("/docs/");
-    }
 }
