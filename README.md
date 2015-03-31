71abe15ad4fc31f55d6f7913f14ace6b8a05528a
William Santasiero <wsantasiero@advanc-ed.org>
2015-03-31 09:23:15 -0400

    update application

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring_boot_swagger_ui
+
+The swagger ui project was dropped into the docs folder.
+
+SwaggerConfiguartion is then used to plumb swagger ui mvc into the Spring Boot application.
+
+The remainder of the application is a standard bare bones Spring Boot app.
--- src/main/java/org/advanced/swaggerui/config/StaticResourceConfiguration.java
+++ src/main/java/org/advanced/swaggerui/config/StaticResourceConfiguration.java
@@ -8,4 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**").addResourceLocations("/docs/");
+    }
 }
